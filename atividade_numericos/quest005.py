8e4b81bf75d018e91a87ea1f57fee4fd3f65eab0
ENdrew Sousa <endrew.developer@gmail.com>
2023-04-26 12:59:29 -0300

    atividade numericos

---
+++ atividade_numericos/quest005.py
@@ -0,0 +1,6 @@
+lagura = float(input('Lagira da parede:\n'))
+altura = float(input('Altura da parede:\n'))
+soma_parede = lagura * altura
+tinta = soma_parede / 2
+print(f'Sua parede te a dimensão de {lagura}x{altura}e sua área é de {soma_parede}m²')
+print(f'Para pintar esse parede {soma_parede}m² vai precisa de {tinta}l de tintar')
