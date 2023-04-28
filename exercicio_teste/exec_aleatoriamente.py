21558f0a86d655670136c06a6ac9f7d92a4b6ac9
Endrew Sousa <endrew.developer@gmail.com>
2023-04-28 15:03:51 -0300

    exercicio teste

--- exercicio_teste/exec_aleatoriamente.py
+++ exercicio_teste/exec_aleatoriamente.py
@@ -1,10 +1,24 @@
+# import random
+# pessoas = []
+
+# nome = input('Digite alunos separa por virgular: ')
+# pessoas = nome.split(',')
+
+# print(f'listas dos alunos {pessoas}')
+
+# aleatoriamente = random.choice(pessoas)
+# print(f'Aluno escolhido aleatoriamente {aleatoriamente}')
+
+# or like this
+
 import random
-pessoas = []
 
-nome = input('Digite alunos separa por virgular: ')
-pessoas = nome.split(',')
+aluno = []
 
-print(f'listas dos alunos {pessoas}')
+for i in range(4):
+    alunos2 = input('Digite os nomes quatros alunos:\n')
 
-aleatoriamente = random.choice(pessoas)
-print(f'Aluno escolhido aleatoriamente {aleatoriamente}')
+aluno.append(alunos2)
+# aleatoriamente = random.choice[aluno]
+
+print(f'Aluno(a) escolhindo aleatoriamente: {random.choice[aluno]}')
