669efd126fee91eeefb4966a3c11284259559598
Endrew Sousa <endrew.developer@gmail.com>
2023-05-08 18:19:23 -0300

    arividade03_listas_e_tuplas

---
+++ atividade03_listas_e_tuplas/quest3.py
@@ -0,0 +1,19 @@
+valores = []
+pares = []
+impares = []
+
+print("Ser quiser para de digitir apena digite 'FIM'")
+while True:
+    numericos = input('Digite qualquer valor:\n')
+    if numericos == "FIM" or numericos == "Fim" or numericos == "fim":
+        break
+
+    valores.append(int(numericos))
+for i in valores:
+    if i % 2 == 0:
+        pares.append(i)
+    else:
+        impares.append(i)
+
+print(f'A lista de pares:{pares}')
+print(f'A lista de impares:{impares}')
