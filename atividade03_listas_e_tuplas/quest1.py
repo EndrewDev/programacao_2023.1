d7967759fcd99d08e94de5488b38e27bb02ccdc7
Endrew Sousa <endrew.developer@gmail.com>
2023-05-08 12:37:09 -0300

    atividade03_lista_e_tuplas

---
+++ atividade03_listas_e_tuplas/quest1.py
@@ -0,0 +1,3 @@
+listas_times = ('Atlético-PE', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragantino',              'Fluminense', 'Amperica-MG', 'Atléticos-GO', 'Santos', 'Ceará SC', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude', 'Grêmio', 'Bahia', 'Sport Recife', 'Chapecoense')
+
+print(f'Os primeiros são {t}')
