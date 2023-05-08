d881b8bb45716d931572814da370462e0d1afdb5
Endrew Sousa <endrew.developer@gmail.com>
2023-05-08 13:01:36 -0300

    atividade03_lista_e_tuplas

--- atividade03_listas_e_tuplas/quest1.py
+++ atividade03_listas_e_tuplas/quest1.py
@@ -1,3 +1,11 @@
-listas_times = ('Atlético-PE', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragantino',              'Fluminense', 'Amperica-MG', 'Atléticos-GO', 'Santos', 'Ceará SC', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude', 'Grêmio', 'Bahia', 'Sport Recife', 'Chapecoense')
+listas_times = ('Atlético-PE', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragantino', 'Fluminense', 'Amperica-MG', 'Atléticos-GO', 'Santos', 'Ceará SC', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude', 'Grêmio', 'Bahia', 'Sport Recife', 'Chapecoense')
 
-print(f'Os primeiros são {t}')
+primeiros_times = listas_times[0:5]
+ultimno_times = listas_times[-4:]
+posicap_palemiras = listas_times.index("Palmeiras")
+estado_riodejaneiro = listas_times[1], listas_times[6]
+
+print(f'Os primeiros são {sorted(primeiros_times)}')
+print(f'Os últimos 4 {sorted(ultimno_times)}')
+print(f'Times do estados do Rio de Janeiro {sorted(estado_riodejaneiro)}')
+print(f'O Palmeiras está na {posicap_palemiras+1} posição.')
