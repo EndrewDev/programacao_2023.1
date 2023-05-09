0bcbd4730e24ff7a3bc659d79dda23d1c234150e
Endrew Sousa <endrew.developer@gmail.com>
2023-05-08 21:44:51 -0300

    atividade04_dic_e_funcao

--- Atividade_04-Dicionários_e_Funções/quest1.py
+++ Atividade_04-Dicionários_e_Funções/quest1.py
@@ -1,20 +1,32 @@
 pessoas = []
 
 print('Se quiser para de digite apena digite "FIM"')
-def IMC(pessoa):
-    while True:
-        nome = input('Digite seu nome:\n')
-        peso = int(input('Digite seu peso:\n'))
-        altura = float(input('Digite a sua altura:\n'))
-        if nome == "FIM" or nome == 'Fim' or nome == 'fim':
-            break
-        if peso == 'FIM' or peso == 'Fim' or peso == 'fim':
-            break
-        if altura == 'FIM' or altura == 'Fim' or altura == 'fim':
-            break
-        pessoas.append({'nome': nome, 'peso': peso, 'altura': altura})
-    
-    imc = peso / altura**2
-    return f'O {nome} seu IMC é {imc}'
+while True:
+    nome = input('Digite seu nome:\n')
+    if nome == "FIM" or nome == 'Fim' or nome == 'fim':
+        break
+    peso = float(input('Digite seu peso:\n'))
+    if peso == 'FIM' or peso == 'Fim' or peso == 'fim':
+        break
+    altura = float(input('Digite a sua altura:\n'))
+    if altura == 'FIM' or altura == 'Fim' or altura == 'fim':
+        break
+    pessoas.append({'nome': nome, 'peso': peso, 'altura': altura})
 
-print(IMC)
+for i in pessoas:
+    i = i
+    imc = peso / altura**2
+    
+
+cadastradas = len(pessoas)
+print(f"Quantas pessoas foram cadastradas: {cadastradas}")
+
+ordenada = []
+for o in 
+ordenada.sort(pessoas)
+print(f"Uma listagem com os nomes das pessoas ordenada por peso: {ordenada}")
+
+
+
+# imc = peso / altura**2
+# print(f'O {nome} seu IMC é {imc}')
