88a4b99b140429b3e223aaa278759367d9e10310
Endrew Sousa <endre.developer@gmail.com>
2023-05-10 16:35:31 -0300

    atividade04_dic_e_funcao

---
+++ Atividade_04-Dicionários_e_Funções/quest2.py
@@ -0,0 +1,15 @@
+pessoas = []
+
+print('Se quiser para de digite apena digite "FIM"')
+while True:
+    nome = input('Digite seu nome:\n')
+    if nome == "FIM" or nome == 'Fim' or nome == 'fim':
+        break
+    peso = int(input('Digite seu peso:\n'))
+    if peso == 'FIM' or peso == 'Fim' or peso == 'fim':
+        break
+    altura = float(input('Digite a sua altura:\n'))
+    if altura == 'FIM' or altura == 'Fim' or altura == 'fim':
+        break
+    imc = peso/(altura)**2
+    pessoas.append({'nome': nome, 'peso': peso, 'altura': altura, 'imc': imc})
