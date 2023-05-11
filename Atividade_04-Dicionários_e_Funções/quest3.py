4155e9cf1c6edd1042f85127df2e1e8ed3f5e7c7
Endrew Sousa <endre.developer@gmail.com>
2023-05-11 09:14:21 -0300

    atividade04_dic_e_funcao

--- Atividade_04-Dicionários_e_Funções/quest3.py
+++ Atividade_04-Dicionários_e_Funções/quest3.py
@@ -1,16 +1,10 @@
-# def peri(c):
-#     p = c * 4
-#     return p
-# perim = float('Digite o valor de perimentro:\n')
-# peri(perim)
-# print(f'Perimentro: {peri}')
-
-def area(larg, comp, c):
+def area(larg, comp):
     a = larg * comp
     p = c * 4
     return a, p
 
 l = float(input('Largura:\n'))
-co = float(input('Comprimento:\n'))
+c = float(input('Comprimento:\n'))
 peri = float(input('Digite o valor de perímetro:\n'))
-print(f'Controle de Terreno {area(l, co)}.\n O controler de perímetro: {area(peri)}')
+area(l, c)
+print(f'Controle de Terreno {area(l, c)}.')
