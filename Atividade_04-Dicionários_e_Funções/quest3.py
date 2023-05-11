d87815caeecc8cf036e901411d06907dc4a42e3e
Endrew Sousa <endre.developer@gmail.com>
2023-05-11 08:48:09 -0300

    atividade04_dic_e_funcao

--- Atividade_04-Dicionários_e_Funções/quest3.py
+++ Atividade_04-Dicionários_e_Funções/quest3.py
@@ -1,11 +1,20 @@
-def area(calcula):
-    a = calcula * calcula
-    return a
+# def area(calcula):
+#     a = calcula * calcula
+#     return a
 
-lado = input('Digite o lado do quadrado:\n')
-area(lado)
-print(f'Area: {area}')
+# lado = input('Digite o lado do quadrado:\n')
+# area(lado)
+# print(f'Area: {area}')
 
-def peri(calculador):
-    p = calculador * 4
-    return p
+# def peri(calculador):
+#     p = calculador * 4
+#     return p
+
+def area(larg, comp):
+    a = larg * comp
+    return f'A área de um terreno {larg}x{comp} e de {a}m².'
+
+l = float(input('Largura:\n'))
+c = float(input('Comprimento:\n'))
+area(l, c)
+print(f'Controle de Terreno {area}.')
