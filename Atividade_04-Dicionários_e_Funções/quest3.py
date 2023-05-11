f1615ae033b72ffa843ef256dc4e378043a94789
Endrew Sousa <endre.developer@gmail.com>
2023-05-11 08:36:34 -0300

    atividade04_dic_e_funcao

---
+++ Atividade_04-Dicionários_e_Funções/quest3.py
@@ -0,0 +1,11 @@
+def area(calcula):
+    a = calcula * calcula
+    return a
+
+lado = input('Digite o lado do quadrado:\n')
+area(lado)
+print(f'Area: {area}')
+
+def peri(calculador):
+    p = calculador * 4
+    return p
