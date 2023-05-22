8cb2e1f6ef1e1c7f0554070967d7c7dfcd102cee
Endrew Sousa <endre.developer@gmail.com>
2023-05-22 11:55:24 -0300

    arquivo

--- arquivos/test1.py
+++ arquivos/test1.py
@@ -1,12 +1,22 @@
 import pickle
 
-nomeegmail = {'nome':'endrew', 'email':'endrew.sousa@escolar.ifrn.edu.br',
-'nome':'werdne', 'email':'wernde.sousa@gmail.com',
-'nome':'endy', 'email':'endy.sousa@gmailm.com'}
+nomegmail = {}
 
-nomesemail = open('nomeegmail.pkl', 'wb')
+while True:
+    nomeemail = input('Digite nome e email por virgula: ')
+    if nomeemail == 'END' or nomeemail == 'end':
+        break
+    nomeemail += nomeemail
+print(nomeemail)
 
-pickle.dump(nomeegmail, nomesemail)
-nomesemail.close()
 
-print(nomesemail)
+# nomeegmail = {'nome':'endrew', 'email':'endrew.sousa@escolar.ifrn.edu.br',
+# 'nome':'werdne', 'email':'wernde.sousa@gmail.com',
+# 'nome':'endy', 'email':'endy.sousa@gmailm.com'}
+
+# nomesemail = open('nomeegmail.pkl', 'wb')
+
+# pickle.dump(nomeegmail, nomesemail)
+# nomesemail.close()
+
+# print(nomesemail)
