05c12e249efedd808b58d1e063eaeec1a4a5bb26
Endrew Sousa <endre.developer@gmail.com>
2023-05-22 11:47:07 -0300

    arquivo

--- arquivos/test1.py
+++ arquivos/test1.py
@@ -1,3 +1,9 @@
 import pickle
 
+nomeegmail = {'nome':'endrew', 'email':'endrew.sousa@escolar.ifrn.edu.br',
+'nome':'werdne', 'email':'wernde.sousa@gmail.com',
+'nome':'endy', 'email':'endy.sousa@gmailm.com'}
+
 nomesmail = open('nomeegmail.pkl', 'wr')
+
+
