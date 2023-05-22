5f525d6079ae665ddd5d82c943c39f4f007e7023
Endrew Sousa <endre.developer@gmail.com>
2023-05-22 11:49:12 -0300

    arquivo

--- arquivos/test1.py
+++ arquivos/test1.py
@@ -4,6 +4,9 @@ nomeegmail = {'nome':'endrew', 'email':'endrew.sousa@escolar.ifrn.edu.br',
 'nome':'werdne', 'email':'wernde.sousa@gmail.com',
 'nome':'endy', 'email':'endy.sousa@gmailm.com'}
 
-nomesmail = open('nomeegmail.pkl', 'wr')
+nomesemail = open('nomeegmail.pkl', 'wb')
 
+pickle.dump(nomeegmail, nomesemail)
+nomesemail.close()
 
+print(nomesemail)
