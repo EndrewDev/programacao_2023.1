e0efa7a2843397a667a27a2f7cef8e356af6582e
Endrew Sousa <endre.developer@gmail.com>
2023-05-22 11:40:20 -0300

    arquivo

--- arquivos/test1.py
+++ arquivos/test1.py
@@ -1,12 +1,3 @@
-nomesegmail = []
+import pickle
 
-while True:
-    nome = input('Digite seu nome:\n')
-    email = input('Digite seu email:\n')
-    if nome == 'END' or nome == 'end':
-        break
-    nome += nomesegmail
-    if email == 'END' or email == 'end':
-        break
-    email += nomesegmail
-
+nomesmail = open('nomeegmail.pkl', 'wr')
