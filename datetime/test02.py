0a7484a4e998de1874c822690119d20c0e7694bc
Endrew Sousa <endrew.developer@gmail.com>
2023-05-25 20:18:55 -0300

    datetime

--- datetime/test02.py
+++ datetime/test02.py
@@ -3,12 +3,15 @@
 # próximo aniversário
 
 import datetime
+from re import I
 
 data_nasc = []
 day = input('Digite seu nascimento formato dd/mm/aaaa: ')
 data_nasc = day.split('/')
+for i in data_nasc:
+    i = i
 
-nasc_endy = datetime.date(data_nasc)
+nasc_endy = datetime.date(i)
 falta = datetime.datetime.now() - nasc_endy
 print(f'Data nascimento: {nasc_endy}')
 print(f'Quantas dias faltas para próximo aniversario: {falta}')
