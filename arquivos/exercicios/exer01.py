29213631aa1d4d946c5186a9f6cc7c469900c9a8
Endrew Sousa <endre.developer@gmail.com>
2023-05-26 14:26:42 -0300

    arquivo

--- arquivos/exercicios/exer01.py
+++ arquivos/exercicios/exer01.py
@@ -1,3 +1,5 @@
+# Faça um programa que contém uma função para calcular juros compostos. A função recebe três parâmetros: montante inicial, rendimento mensal e a quantidade de meses; e deve retornar o valor do montante final. Ou seja, a função calcula qual é o valor final de uma aplicação, dado o valor inicial, o rendimento mensal da aplicação e a quantidade de meses. O programa deverá solicitar essas informações do usuário e executar a função para calcular o valor final e imprimir o resultado na tela.
+
 montante_inicaial = []
 rendimento_mesnual = []
 qunatidade_meses = []
