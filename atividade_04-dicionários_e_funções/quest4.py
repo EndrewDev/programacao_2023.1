a4c9abe2b4600184fbac11fbaf747bcaacf167e5
Endrew Sousa <endre.developer@gmail.com>
2023-05-26 14:28:18 -0300

    atividade04_dic_e_funcao

--- atividade_04-dicionários_e_funções/quest4.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n = int(input("Digite o valor de n:\n"))
-    fat = 1
-    i = 2
-    while i <= n:
-        fat = fat*i
-        i = i + 1
-    print(f"O valor de {n} eh = {fat}")
-main()
