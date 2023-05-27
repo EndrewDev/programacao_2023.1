f39ddd62234251e5bf16f00b328b936280f3b991
Endrew Sousa <endre.developer@gmail.com>
2023-05-27 14:19:00 -0300

    arquivo

--- arquivos/exercicios/exer01.py
+++ arquivos/exercicios/exer01.py
@@ -1,8 +1,11 @@
 # Faça um programa que contém uma função para calcular juros compostos. A função recebe três parâmetros: montante inicial, rendimento mensal e a quantidade de meses; e deve retornar o valor do montante final. Ou seja, a função calcula qual é o valor final de uma aplicação, dado o valor inicial, o rendimento mensal da aplicação e a quantidade de meses. O programa deverá solicitar essas informações do usuário e executar a função para calcular o valor final e imprimir o resultado na tela.
 
-juro_comp = []
+import pickle
 
 def juro_composto(i, j, m):
+    mont_in = []
+    rend_m = []
+    quant_m = []
     calc = i * pow((1 + j), m)
     calc = calc - i
     return f'{calc}'
