6d74e339c0cbc3356bcc574f1445791a6bb4b173
Endrew Sousa <endre.developer@gmail.com>
2023-05-29 13:14:50 -0300

    arquivo

---
+++ arquivos/exercicios/exer04.py
@@ -0,0 +1,10 @@
+from datetime import date, timedelta
+
+def transforma_data(datas):
+    x = datetime.datetime(datas)
+
+
+
+samanas = input('Digite semanas: ')
+mes = input('Digite o nome de mes: ')
+anos = int(input('Digite o ano: '))
