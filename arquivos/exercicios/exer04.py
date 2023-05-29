11028b18e9ca5cc7f5f40a18d9a238546620256f
Endrew Sousa <endre.developer@gmail.com>
2023-05-29 14:00:54 -0300

    arquivo

--- arquivos/exercicios/exer04.py
+++ arquivos/exercicios/exer04.py
@@ -1,10 +1,15 @@
-from datetime import date, timedelta
+import datetime
 
-def transforma_data(datas):
-    x = datetime.datetime(datas)
+def transforma_datas(data):
+    listas_semnas = []
+    x = datetime.datetime(data)
+    semanas = x.weekday()
+    dias = x.day
+    anos = x.year
+    meses = x.month
+    listas_semnas.append({'dias': dias, 'semanas': semanas, 'mês': meses, 'anos': anos})
 
+datas = datetime.datetime(2020, 4, 6)
+respostas = transforma_datas(datas)
 
-
-samanas = input('Digite semanas: ')
-mes = input('Digite o nome de mes: ')
-anos = int(input('Digite o ano: '))
+print(respostas)
