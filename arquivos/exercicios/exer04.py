5a4024ff47b55606d4fe34c4f085804fe1972291
Endrew Sousa <endrew.developer@gmal.com>
2023-05-30 17:26:04 -0300

    arquivIo

--- arquivos/exercicios/exer04.py
+++ arquivos/exercicios/exer04.py
@@ -1,16 +1,29 @@
 from datetime import date
 import pickle
-# import calendar
+import calendar
 
 # print(calendar.month(2020, 4))
 
 def transforma_datas(datainicio):
-    comeca = date(datainicio).weekday(6)
-    for dia in comeca:
-        dias = 0
-        if dia == comeca:
-            dias += 1
-        final = print(f'Tens qunatas dias de semanas: {dias}')
+    comeca = date(datainicio)
+    listas_semanas = ['segunda-feira', 'terça-feira', 'quarta-feira', 'quieIra-feira', 'sexta-feira', 'sábado', 'domingo']
+    seegundas = []
+    tercas = []
+    quartas = []
+    quintas = []
+    sextas = []
+    sabados = []
+    domingos = []
+    if listas_semanas[0] == comeca.weekday(0):
+        seegundas.append(comeca.weekday(0))
+    elif listas_semanas[1] == comeca.weekday(1):
+        tercas.append(comeca.weekday(1))
+    elif listas_semanas[2] == comeca.weekday(2):
+        quartas.append(comeca.weekday(2))
+    elif listas_semanas[3] == comeca.weekday(3):
+        quintas.append(comeca.weekday(3))
+    elif
+    final = print(f'Tens qunatas dias de semanas: {dias}')
     return final
 
 data_comeca = date(2020, 4, 5)
