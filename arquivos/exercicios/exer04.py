fa06659c8803d5e170dfde1d371533658b4c18b2
Endrew Sousa <endrew.developer@gmal.com>
2023-05-30 09:44:01 -0300

    arquivo

--- arquivos/exercicios/exer04.py
+++ arquivos/exercicios/exer04.py
@@ -4,14 +4,15 @@ import pickle
 
 # print(calendar.month(2020, 4))
 
-def transforma_datas(data):
-    x = date(data).isocalendar()
-    final = print(f'Quantas tens os dias de semanas: {x}')
-    return final
+def transforma_datas(datainicio, datafinal):
+    comeca = date(datainicio).isocalendar()
+    final = date(datafinal).isocalendar()
+    return f'Tens quantas dais dos domingos: {comeca} e {final}'
 
-datas = date(2020, 4, 4)
+data_comeca = date(2020, 4, 6)
+data_final = date(2020, 4, 6)
 
-respostas = transforma_datas(datas)
+respostas = transforma_datas(data_comeca, data_final)
 
 print(respostas)
 
