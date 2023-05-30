d77a57ec001fcbafbee7c520d2c58bb575f5664c
Endrew Sousa <endrew.developer@gmal.com>
2023-05-30 09:33:42 -0300

    arquivo

--- arquivos/exercicios/exer04.py
+++ arquivos/exercicios/exer04.py
@@ -1,21 +1,19 @@
-import datetime
+from datetime import date
 import pickle
 # import calendar
 
 # print(calendar.month(2020, 4))
 
 def transforma_datas(data):
-    listas_datas = []
-    for i in data:
-        listas_datas.append(i)
-    data = datetime.datetime(listas_datas)
-    for seamana in data:
-        if seamana == data.weekday(6):
-            print(f"Quantas tem os dias dos seamana: {data}")
-    return
+    x = date(data).isocalendar()
+    final = print(f'Quantas tens os dias de semanas: {x}')
+    return final
 
-datas = datetime.date(2020, 4, 5)
+datas = date(2020, 4, 4)
 
 respostas = transforma_datas(datas)
 
 print(respostas)
+
+with open('data.pick.txt', 'wb') as f:
+    dado = pickle.load(respostas, f)
