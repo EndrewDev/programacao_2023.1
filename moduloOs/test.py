d26f73c49807f7c4205b11dc36cbfc697add6bf2
Endrew Sousa <endrew.developer@gmail.com>
2023-06-06 19:25:29 -0300

    erro

---
+++ moduloOs/test.py
@@ -0,0 +1,13 @@
+import os
+
+
+diretorio = input('Digtite um nome de diretorio para quer achar: ')
+if diretorio != 
+documentos = input('Digite um nome de documentos para quer entrar ')
+text = input('Digite um nome de pasta para ler: ')
+
+caminho = (f'{diretorio}/{documentos}/{text}')
+
+caminho1 = os.path.basename(caminho)
+
+print(caminho1)
