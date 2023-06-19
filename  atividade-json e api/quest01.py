5f8d87c9b53fd295f5c6ea907577e755d84e9b91
Endrew Sousa <endrew.developer@gmail.com>
2023-06-19 16:21:48 -0300

    json e api

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -1,13 +1,8 @@
 import httpx
 
 lists_dados = []
-dic_dados = {}
 
-url = httpx.post('https://drive.google.com/file/d/1HXpJd6bj80xBoINQhJFmiZtnqNrgf_i-/view' data={'kay': 'value'})
-# lists_dados.append(url)
+url = httpx.put('https://drive.google.com/file/d/1HXpJd6bj80xBoINQhJFmiZtnqNrgf_i-/view', data={'key': 'value'})
+lists_dados.append(url)
 
-# print(lists_dados)
-
-outrourl = url.url
-
-print(outrourl)
+print(lists_dados)
