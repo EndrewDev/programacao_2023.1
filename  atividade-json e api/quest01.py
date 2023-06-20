60786820c964350b9bd4ef259fc709b0de0a60f5
Endrew Sousa <endrew.developer@gmail.com>
2023-06-19 21:38:12 -0300

    json e api

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -1,4 +1,12 @@
-import csv
-with open('apach_logs.txt.txt') as f:
-    for r in csv.reader(f, delimiter=" "):
-        print(r)
+import json
+
+dados = {}
+
+for i in range(dados):
+    i = json.dumps(i)
+    print(i)
+
+# print(dados1)
+
+# with open('apach_logs.txt') as f:
+#     for i in
