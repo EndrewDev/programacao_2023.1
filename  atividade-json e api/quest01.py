3875878f11983c27f267c69f44055ededfcf11b9
Endrew Sousa <endrew.developer@gmail.com>
2023-06-19 21:48:32 -0300

    json e api

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -1,12 +1,15 @@
 import json
 
-dados = {}
+# dados = {}
 
-for i in range(dados):
-    i = json.dumps(i)
-    print(i)
+# for i in range(dados):
+#     i = json.dumps(i)
+#     print(i)
 
 # print(dados1)
 
-# with open('apach_logs.txt') as f:
-#     for i in
+with open('apach_logs.json', 'r') as openfile:
+    json_objeto = json.load(openfile)
+
+print(json_objeto)
+print(type(json_objeto))
