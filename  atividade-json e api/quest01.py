dc3535f1308e217342a313351ac8b309a70ced5c
Endrew Sousa <endrew.developer@gmail.com>
2023-06-21 15:00:05 -0300

    jsoneapi

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -1,3 +1,8 @@
 with open('apache_logs.txt', 'r',encoding='utf-8') as arq:
     texto = arq.read()
-    print(texto)
+    # print(texto)
+
+lista_dados = []
+lista_dados.append(texto)
+
+dic_dados = {'IP de origem': lista_dados[0], 'identd': lista_dados[1], 'user': lista_dados[2], 'data/hora': lista_dados[3], 'Requisição HTTP': lista_dados[4], 'Código de status HTTP': lista_dados[5], 'Bytes': lista_dados[6], 'Pagina de origem': lista_dados[7], 'Navegador': lista_dados[9]}
