fe068a30376cae7b316eae03052e327257f36700
Endrew Sousa <endrew.developer@gmail.com>
2023-06-21 16:48:03 -0300

    jsoneapi

---  atividade-json e api/quest02.py
+++  atividade-json e api/quest02.py
@@ -1,5 +1,7 @@
-import httpx
+import httpx, json
 
 url = httpx.get('https://cdn.apicep.com/file/apicep/59012570.json')
 
-print(url)
+with open('cep.json', 'wr') as openurl:
+    json_objeto = json.load(openurl)
+    print(json_objeto)
