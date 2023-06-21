c7ed6615e9db3f8bc2f3218efc43da44e88027a6
Endrew Sousa <endrew.developer@gmail.com>
2023-06-21 14:54:12 -0300

    jsoneapi

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -1,18 +1,3 @@
-# import json
-
-# openfile = open('apache_logs.txt', 'r', encoding='utf-8')
-# print(openfile)
-# dic_dados = {}
-
 with open('apache_logs.txt', 'r',encoding='utf-8') as arq:
     texto = arq.read()
     print(texto)
-
-# with open(openfile) as f:
-#     for i in f:
-#         i,description = i.strip().split(None, 1)
-# dic_dados[i] = description.strip()
-# outro_file = open('teste1.json', 'w')
-# json.dump(dic_dados, outro_file, indent= 4, sort_keys=False)
-# outro_file.close()
-
