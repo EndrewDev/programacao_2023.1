d8a155562bf196dd5b1945bd663d5e171b81957c
Endrew Sousa <endrew.developer@gmail.com>
2023-06-21 09:44:37 -0300

    json e api

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -1,6 +1,6 @@
 import json
 
-with open('apache_logs.txt', 'r+') as openfile:
+with open('apache_logs.txt', 'rt') as openfile:
     json_objeto = json.loads(openfile)
     for i in json_objeto:
         print(i)
