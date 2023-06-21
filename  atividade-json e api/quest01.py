433a057d3c6513f29ce48e9198064eabb80dba8c
Endrew Sousa <endrew.developer@gmail.com>
2023-06-21 14:35:10 -0300

    json e api

---  atividade-json e api/quest01.py
+++  atividade-json e api/quest01.py
@@ -5,4 +5,8 @@ dic_dados = {}
 
 with open(openfile) as f:
     for i in f:
-        i, description = i.strip().split(None, 1)
+        i,description = i.strip().split(None, 1)
+dic_dados[i] = description.strip()
+outro_file = open('teste1.json', 'w')
+json.dump(dic_dados, outro_file, indent= 4, sort_keys=False)
+outro_file.close()
