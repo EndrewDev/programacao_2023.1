f50a877c87f8365eef5d8cc257e35aa31b8d5390
Endrew Sousa <endrew.developer@gmail.com>
2023-06-25 21:04:38 -0300

    jsoneapi

---
+++  atividade-json e api/outra_quest001.py
@@ -0,0 +1,23 @@
+import json, httpx, datetime
+
+lista = []
+dic_cotacao = {}
+
+def cotacao():
+    data = input('Digite a data para receber o documetno da cotação (formatada aaaa-mm-dd): ')
+    lista.append(data)
+    lista1 = lista.split('-')
+    requisicao = httpx.get('https://economia.awesomeapi.com.br/all/USD-BRL/')
+    cotacoa = requisicao.json()
+    for i,v in cotacoa.items():
+        if cotacao['USD']['create_date'] != data:
+            data1 = datetime.datetime(lista[0], lista[1], lista[2])
+            cotacao = dict.pop(cotacao['USD']['create_date'])
+            cotacao = cotacao['USD']['create_date'] = data1
+            print('{}:{}'.format(i, v))
+        else:
+            if cotacao == data:
+                print('{}:{}'.format(i, v))
+    return cotacoa
+
+cotacao()
---  atividade-json e api/quest02.py
+++  atividade-json e api/quest02.py
@@ -3,5 +3,6 @@ import httpx, json
 url = httpx.get('https://ws.apicep.com/cep/59015-000.json')
 url = url.json()
 
-for i,v in url.items():
-    print('{}:{}'.format(i,v))
+print(url)
+# for i,v in url.items():
+#     print('{}:{}'.format(i,v))
