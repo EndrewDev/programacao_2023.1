1b36b329674d56341d6ab21330b1e5caa3466813
Endrew Sousa <endrew.developer@gmail.com>
2023-08-18 22:00:52 -0300

    leetcode

--- LeerCode/test_two_sum.py
+++ LeerCode/test_two_sum.py
@@ -1,9 +1,10 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
          seen = {}
          for i, num in enumerate(nums):
              if target - num in seen:
                  return([seen[target - num], i])
              elif num not in seen:
                  seen[num] = i
-                
+
+nums = [2, 7, 11, 15]
