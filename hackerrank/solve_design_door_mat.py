23e93009aa1a1916c059c7b331134ff2b46363e4
Endrew Sousa <endrew.developer@gmail.com>
2023-08-26 10:23:22 -0300

    hackerrank

--- hackerrank/solve_design_door_mat.py
+++
@@ -1,8 +0,0 @@
-R,C = map(int,input().split(' '))
-
-for i in range(1,R,2):
-    print((".|."*i).center(C, '-'))
-print("WELCOME".center(C,'-'))
-
-for i in range(R-2,-1,-2):
-    print((".|."*i).center(C, '-'))
